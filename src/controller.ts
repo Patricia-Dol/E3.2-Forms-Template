3b3d819dbfea2a56b4b0c8b9d14bd6fe57c66934
Patricia-Dol <pattid1608@gmail.com>
2024-03-18 13:58:21 +0000

    started 3.2 need to finish step 4 (last part)

--- src/controller.ts
+++ src/controller.ts
@@ -9,14 +9,85 @@ export const getHome = async (req: IncomingMessage, res: ServerResponse) => {
 };
 
 // TODO: Copy-paste the getOnePokemon and getAllPokemon functions from the previous exercise.
+export const getNewForm = async (req: IncomingMessage, res: ServerResponse) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(await renderTemplate("src/views/NewFormView.hbs"));
+};
+
+export const getAllPokemon = async (
+    req: IncomingMessage,
+    res: ServerResponse,
+) => {
+    const url = new URL(req.url!, `http://${req.headers.host}`); // Use URL parsing
+    const queryParams = url.searchParams;
+    const typeFilter = queryParams.get("type");
+    const sortBy = queryParams.get("sortBy");
+    let pokemon: Pokemon[] = [];
+
+    // Apply basic filtering if we have a `typeFilter`:
+    if (typeFilter) {
+        pokemon = database.filter((pokemon) => pokemon.type === typeFilter);
+    } else {
+        pokemon = database;
+    }
+
+    if (sortBy === "name") {
+        pokemon = [...pokemon].sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+        JSON.stringify({ message: "All Pokemon", payload: pokemon }, null, 2),
+    );
+};
+
+export const getOnePokemon = async (
+    req: IncomingMessage,
+    res: ServerResponse,
+) => {
+    const id = Number(req.url?.split("/")[2]);
+    const foundPokemon = database.find((pokemon) => pokemon.id === id);
+
+    if (!foundPokemon) {
+        res.statusCode = 404;
+        return res.end(
+            JSON.stringify({ message: "Pokemon not found" }, null, 2),
+        );
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+        JSON.stringify(
+            {
+                message: "Pokemon found",
+                payload: foundPokemon,
+            },
+            null,
+            2,
+        ),
+    );
+};
 
 export const createPokemon = async (
     req: IncomingMessage,
     res: ServerResponse,
 ) => {
     const body = await parseBody(req);
+    //looping through all the data in the database
+    const newPokemon = Object.fromEntries(new URLSearchParams(body).entries());
 
-    res.end(body);
+    //then pushing it into the database
+    database.push({
+    id: database.length + 1,
+    name: newPokemon.name,
+    type: newPokemon.type
+  });
+    res.statusCode = 201;
+    res.setHeader("Content-Type", "text/html");
+    res.end(await renderTemplate("src/views/ListView.hbs"));
 };
 
 const parseBody = async (req: IncomingMessage) => {
